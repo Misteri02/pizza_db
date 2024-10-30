796bc09397ac66d0226ee223578148621f0ad34b
Misteri02 <julius.tuominen02@gmail.com>
2024-10-30 12:18:46 +0200

    database

---
+++ QuickDBD-export.sql
@@ -0,0 +1,85 @@
+﻿
+
+CREATE TABLE `orders` (
+    `row_id` int  NOT NULL ,
+    `order_id` varchar(10)  NOT NULL ,
+    `created_at` datetime  NOT NULL ,
+    `item_id` varchar(10)  NOT NULL ,
+    `quantity` int  NOT NULL ,
+    `cust_id` int  NOT NULL ,
+    `delivery` boolean  NOT NULL ,
+    `add_id` int  NOT NULL ,
+    PRIMARY KEY (
+        `row_id`
+    )
+);
+
+CREATE TABLE `customers` (
+    `cust_id` int  NOT NULL ,
+    `cust_firstname` varchar(50)  NOT NULL ,
+    `cust_lastname` varchar(50)  NOT NULL ,
+    PRIMARY KEY (
+        `cust_id`
+    )
+);
+
+CREATE TABLE `address` (
+    `add_id` int  NOT NULL ,
+    `delivery_address1` varchar(200)  NOT NULL ,
+    `delivery_address2` varchar(200)  NULL ,
+    `delivery_city` varchar(50)  NOT NULL ,
+    `delivery_zipcode` varchar(20)  NOT NULL ,
+    PRIMARY KEY (
+        `add_id`
+    )
+);
+
+CREATE TABLE `item` (
+    `item_id` varchar(10)  NOT NULL ,
+    `sku` varchar(20)  NOT NULL ,
+    `item_name` varchar(100)  NOT NULL ,
+    `item_cat` varchar(100)  NOT NULL ,
+    `item_size` varchar(10)  NOT NULL ,
+    `item_price` decimal(5,2)  NOT NULL 
+);
+
+CREATE TABLE `ingredient` (
+    `ing_id` varchar(10)  NOT NULL ,
+    `ing_name` varchar(100)  NOT NULL ,
+    `ing_weight` int  NOT NULL ,
+    `ing_meas` varchar(20)  NOT NULL ,
+    `ing_price` decimal(5,2)  NOT NULL 
+        PRIMARY KEY (
+        `ing_id`
+    )
+);
+
+    CREATE TABLE `recipe` (
+    `row_id` int  NOT NULL ,
+    `recipe_id` varchar(20)  NOT NULL ,
+    `ing_id` varchar(10)  NOT NULL ,
+    `quantity` int  NOT NULL ,
+        PRIMARY KEY (
+        `row_id`
+    )
+);
+
+    CREATE TABLE `inventory` (
+    `inv_id` int  NOT NULL ,
+    `item_id` varchar(10)  NOT NULL ,
+    `quantity` int  NOT NULL ,
+        PRIMARY KEY (
+        `inv_id`
+    )
+);
+
+
+ALTER TABLE `customers` ADD CONSTRAINT `fk_customers_cust_id` FOREIGN KEY(`cust_id`)
+REFERENCES `orders` (`cust_id`);
+
+ALTER TABLE `address` ADD CONSTRAINT `fk_address_add_id` FOREIGN KEY(`add_id`)
+REFERENCES `orders` (`add_id`);
+
+ALTER TABLE `item` ADD CONSTRAINT `fk_item_item_id` FOREIGN KEY(`item_id`)
+REFERENCES `orders` (`item_id`);
+
